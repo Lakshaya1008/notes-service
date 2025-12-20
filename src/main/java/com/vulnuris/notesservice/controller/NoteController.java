ed849a33f901c7a9d78455b2bc4f92f703c0e377
Lakshaya1008 <lakshayajain93@gmail.com>
2025-12-20 15:47:02 +0530

    Add Note REST controller with CRUD APIs

---
+++ src/main/java/com/vulnuris/notesservice/controller/NoteController.java
@@ -0,0 +1,50 @@
+package com.vulnuris.notesservice.controller;
+
+import com.vulnuris.notesservice.model.Note;
+import com.vulnuris.notesservice.service.NoteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notes")
+public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteService.createNote(note);
+    }
+
+    @GetMapping
+    public List<Note> getAllNotes() {
+        return noteService.getAllNotes();
+    }
+
+    @GetMapping("/{id}")
+    public Note getNoteById(@PathVariable Long id) {
+        return noteService.getNoteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Note updateNote(
+            @PathVariable Long id,
+            @Valid @RequestBody Note note
+    ) {
+        return noteService.updateNote(id, note);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteNote(@PathVariable Long id) {
+        noteService.deleteNote(id);
+    }
+}
